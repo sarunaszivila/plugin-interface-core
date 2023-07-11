7f0832f3fc9fd61f79e1296d058ccf2f3f11512d
sarunaszivila <77277937+sarunaszivila@users.noreply.github.com>
2023-07-11 14:00:59 +0300

    Tag model

---
+++ Models/TagMatching.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PlentymarketsShopwareCore\Models;
+
+use PlentymarketsShopwareCore\Configuration\PluginConfiguration;
+
+/**
+ * Class TagMatching
+ *
+ * @property int    $id
+ * @property int    $configuration_id
+ * @property string $sw_tag_id
+ * @property int    $tag_id
+ */
+class TagMatching
+{
+
+}
